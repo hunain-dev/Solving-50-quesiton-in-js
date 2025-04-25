0eac6eefc621f488e2a022abea5f2522a6a79e15
hunain soomro <huniiali1122@gmail.com>
2025-04-26 03:08:52 +0500

    39 quesiton completed

--- script.js
+++ script.js
@@ -1,1 +1,188 @@
-alert('hey haunin')
+//Basic Console usage (10 questions)
+
+//  console.log('my name is hunain khan and my fav hooby is cooding')
+
+// console.log(45 * 2 -10)
+
+// const year = new Date();
+// console.log(year.getFullYear());
+
+
+// const firstname = 'hunain';
+// const lastName = 'khan';
+
+// console.log(firstname,lastName);
+
+// var value = 10;
+// console.log(value);
+// value = 30
+// console.log(value)
+
+
+// console.error('this is an error message')
+
+
+// const square = 12;
+// console.log(square * square);
+
+
+// const values = true;
+// console.log(typeof values);
+
+
+// const age = 30;
+
+// if (age > 18) {
+//     console.log('you are an adult');
+
+    
+// } else {
+//     console.log('you are not an adult');
+// }
+
+// console.log(100/0)
+
+
+
+//Variable and datatype (10 questions)
+
+
+// let value = 10;
+// console.log(value)
+
+
+// const values = Math.PI;
+// console.log(values)
+
+
+// let reasing = 30;
+// console.log(reasing);
+// reasing = 40;
+// console.log(reasing)
+
+
+// const val = null;
+// console.log(typeof val)
+
+
+// const string = 'hunain';
+// console.log(typeof string)
+
+
+// const boolen = false;
+// console.log(typeof boolen)
+
+// const strings = 'hunain khan';;
+// const number = 30;
+// const boolean = true;
+
+// console.log(strings, number, boolean);
+
+
+// var vari;
+// console.log(typeof vari);
+
+
+// const a = undefined;
+// console.log(typeof a);
+
+// const arr = [1, 2, 3, 4, 5];
+
+
+//Loops (10 questions)
+
+
+// for(i = 0; i < 51; i++){
+//     console.log(i)
+// }
+
+
+// const str  = 'javascript';
+// for (const element of str) {
+//     console.log(element);
+// }
+
+// for (let i = 0; i < 21; i++) {
+//     if (i  % 2 !== 0) {
+//         console.log(i + ' is odd number');
+        
+//     }
+ 
+// }
+
+
+
+// let arr = [1, 2, 3, 4];
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr.reverse()[i]);
+    
+// }
+
+
+// const obj = [
+//     {name: 'hunain', age: 20},
+//     {name: 'khan', age: 30},
+//     {name: 'ali', age: 40}
+// ];
+
+
+// for (const key in obj) {
+//     console.log(key, obj[key]);
+
+  
+// }
+
+
+// obj.forEach((element) =>    {
+//     console.log(element.name);
+//     console.log(element.age);
+// })
+
+
+////arrays (10 questions)
+
+
+// let arr = ['spiderman', 'batman', 'superman', 'ironman'];
+// console.log(arr);
+
+// arr.forEach((Element)=>{
+//     console.log(Element)
+// })
+
+
+// const elem = ['spiderman', 'batman', 'superman', 'ironman'];
+// console.log(elem[1]);
+
+
+// let arr = ['spiderman', 'batman', 'superman', 'ironman'];
+// arr.unshift('hulk', 'thor', 'captain america');
+// console.log(arr)
+
+
+// let arr = ['spiderman', 'batman', 'superman', 'ironman'];
+
+// arr.pop();
+// console.log(arr)
+
+
+// const arr = ['spiderman', 'batman', 'superman', 'ironman'];
+
+// arr.slice(1, 3);
+// console.log(arr);
+
+
+// const arr = [1,2,3,4,5,6,7,8,9,10];
+// const bc = arr.indexOf(5);
+// console.log(bc);
+
+
+// const arr = [1,2,3,4,5,6,7,8,9,10];;
+// console.log(arr.includes(1));
+
+
+// `const onearray = [1,2,3,4,5,6,7,8,9,10];
+// const twarray = [11,12,13,14,15,16,17,18,19,20];
+
+// const combine = onearray.concat(twarray);
+// console.log(combine);`
